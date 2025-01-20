d8587038b90ece6fa7b2988eb145b53f93d7b8d7
Hanna-Jeanne <janghanna423@gmail.com>
2025-01-20 10:39:09 +0900

    likelion-vite-react 저장소 전체 다운로드

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,7 @@
+import Prose from '@/components/prose';
+
 function App() {
-  return (
-    <div className="prose">
-      <header>
-        <h1 lang="en">Custom Vite Template</h1>
-      </header>
-      <main>
-        <p>React 프로젝트 시작을 위한 사용자 정의 템플릿</p>
-      </main>
-      <footer>
-        <small lang="en">
-          &copy; {new Date().getFullYear()} EUID. All rights reserved.
-        </small>
-      </footer>
-    </div>
-  );
+  return <Prose />;
 }
 
 export default App;
